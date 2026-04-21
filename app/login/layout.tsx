f8c920b5a9367b3ef7f8d8840285be408481c43c
Agnieszka26 <medrek.agn@yahoo.com>
2026-04-21 16:30:08 +0200

    fix: show up beautiful share link

--- app/login/layout.tsx
+++ app/login/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Montserrat, Qwigley } from "next/font/google";
 import "../../app/globals.css";
 
+const siteUrl = new URL("https://www.piesnporanka.pl");
 
 const montserrat = Montserrat({
   variable: "--font-montserrat",
@@ -15,9 +16,34 @@ weight: "400"
 })
 
 export const metadata: Metadata = {
-  title: "Pieśń Poranka - odpoczywam w górach ",
+  metadataBase: siteUrl,
+  title: "Pieśń Poranka - odpoczywam w górach",
   description:
     "Oferujemy Państwu pobyt w domku wypoczynkowym w górach położonym na malowniczej działce na wysokości 740m n.p.m. we wsi Łysina, w Beskidzie Małym na grani Ścieszków Gronia.",
+  openGraph: {
+    type: "website",
+    url: "/login",
+    title: "Pieśń Poranka - odpoczywam w górach",
+    description:
+      "Oferujemy Państwu pobyt w domku wypoczynkowym w górach położonym na malowniczej działce na wysokości 740m n.p.m. we wsi Łysina, w Beskidzie Małym na grani Ścieszków Gronia.",
+    siteName: "Pieśń Poranka",
+    locale: "pl_PL",
+    images: [
+      {
+        url: "/og/og-image.png",
+        width: 1200,
+        height: 630,
+        alt: "Pieśń Poranka - odpoczywam w górach",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Pieśń Poranka - odpoczywam w górach",
+    description:
+      "Oferujemy Państwu pobyt w domku wypoczynkowym w górach położonym na malowniczej działce na wysokości 740m n.p.m. we wsi Łysina, w Beskidzie Małym na grani Ścieszków Gronia.",
+    images: ["/og/og-image.png"],
+  },
 };
 
 export default function RootLayout({
@@ -26,7 +52,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="pl">
       <body className={` ${qwigley.variable} ${montserrat.variable} antialiased`}>
        
         {children}
